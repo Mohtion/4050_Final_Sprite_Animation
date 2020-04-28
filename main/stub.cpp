9e94167e1e9b61105987cc7238996848905c046f
ejlowe <48259064+ejlowe@users.noreply.github.com>
2020-04-27 21:13:42 -0400

    Update stub.cpp

--- main/stub.cpp
+++ main/stub.cpp
@@ -210,7 +210,7 @@ void drawSurfaceOfRevolution()
 
 void keyboardFunction(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-   if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+   if (key == GLFW_KEY_E && action == GLFW_PRESS)
    {
       if(count == 0)
       {
