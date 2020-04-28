c3b24172ce55c10eff9ecd7e6c8d28ae0c3fda4e
ejlowe <48259064+ejlowe@users.noreply.github.com>
2020-04-27 21:10:08 -0400

    Add files via upload
    Mostly works, hold down E to animate

--- main/frag.glsl
+++ main/frag.glsl
@@ -1,28 +1,17 @@
 #version 410
-uniform mat4 proj_mat, view_mat, model_mat;
-uniform vec3 user_light;
-uniform bool spec_on, texture_on, diffuse_on;
-uniform float user_spec;
-//uniform sampler2D Image;
-// Light position, in camera coordinates
-//vec3 LightPosition = vec3(view_mat);
-// The color of our light
-//vec3 LightColor = vec3(1.0, 1.0, 1.0);
-// And from the uniform outputs for the textures setup in main.cpp.
+
+
+// Define INPUTS from fragment shader
+
 uniform sampler2D texture00;
-uniform sampler2D texture01;
 
-smooth in vec2 TextCoordInterp;
-smooth in vec3 PositionInterp;
-out vec4 FBColor;
-void main()
-{
-    //texture
-    //vec4 textureColor = texture(texture00, TextCoordInterp);
-    //specular contribution
+in vec2 tex1;
 
-    /* Apply the mask to the specular contribution. The "1.0 –" is
-    To invert the texture's alpha channel */
-    //vec3 lightContribution = vec3(textureColor.a;// – 0.5);//textureColor.a);
-    FBColor = texture(texture00, TextCoordInterp);//vec4(textureColor.rgba);
+out vec4 fragment_color; //RGBA color
+void main () {
+
+   vec4 texColor = texture(texture00, tex1);
+   vec3 texture = vec3(normalize(texColor));
+
+   fragment_color = vec4(texture, 0.0);
 }
